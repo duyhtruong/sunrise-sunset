edcba95dbe40643f7d51b13ce75b40f5b3bd26b4
Duy Truong <41127922+duyhtruong@users.noreply.github.com>
2018-07-23 10:49:01 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,79 +1,89 @@
+/* 	This application takes in a user input for location. 
+	Expected input may be Address, city, zipcode, or point of interest.
+	The application will output the local sunrise and sunset time.
+*/
 
 
-//click
-document.getElementById("city")
+document.getElementById("userInput")
 	.addEventListener("keypress", function(event) {
-    if (event.keyCode == 13){
-    event.preventDefault();
+    	if (event.keyCode == 13){
+    	event.preventDefault();
 
-//geocoder API call with user INPUT    
-var geocoder = new google.maps.Geocoder();
-var loc = document.getElementById('city').value;
-geocoder.geocode( { "address": loc}, function(results, status) {
-    if (status == google.maps.GeocoderStatus.OK && results.length > 0) {
-        var location = results[0].geometry.location;
-        lat = location.lat();
-        lng = location.lng();
+			//Retrieve longitude and latitude (geocode) of user inputted location using google maps   
+			var geocoder = new google.maps.Geocoder();
+			var userLocation = document.getElementById('userInput').value;
+			geocoder.geocode( { "address": userLocation}, function(results, status) {
+    			if (status == google.maps.GeocoderStatus.OK && results.length > 0) {
+        			var location = results[0].geometry.location;
+        			lat = location.lat();
+        			lng = location.lng();
      
-//sunrise API call with lat and long of user INPUT       
-        var endpoint = 'https://api.sunrise-sunset.org/json?';
-		var latt = 'lat='+ lat +'&';
-		var longg = 'lng='+ lng;
-		var formatted = '&formatted=0'
-		var complete_endpoint = endpoint + latt + longg + formatted;
+					//Retrieve sunset and sunrise time of input latitude and longitude using sunrise API      
+        			var sunriseEndpoint = 'https://api.sunrise-sunset.org/json?';
+					sunriseEndpoint += 'lat=' + lat + '&' + 'lng=' + lng + '&formatted=0';
 
 
-		var request = new XMLHttpRequest();
-		request.open('GET', complete_endpoint)
-		request.onload = function(){
-				var json = JSON.parse(request.responseText);
-				var sunriseTime = new Date(json.results.sunrise);
-				var sunsetTime = new Date(json.results.sunset);
-				var unixsunsetTime = JSON.stringify(sunsetTime.getTime()) / 1000;
-				var unixsunriseTime = JSON.stringify(sunriseTime.getTime()) / 1000;
+					//XMLHttpRequest to sunrise API
+					var request = new XMLHttpRequest();
+					request.open('GET', sunriseEndpoint)
+					request.onload = function(){
+						var json = JSON.parse(request.responseText);
+
+						//Creating Date objects of sunrise and sunset times and converting to unix time
+						var sunriseTime = new Date(json.results.sunrise);
+						var sunsetTime = new Date(json.results.sunset);
+						var unixsunsetTime = JSON.stringify(sunsetTime.getTime()) / 1000;
+						var unixsunriseTime = JSON.stringify(sunriseTime.getTime()) / 1000;
 				
 
 
-// call timezone API to get timezone of user input to get difference
-				var timezoneAPI = 'https://maps.googleapis.com/maps/api/timezone/json?location='
-				var apikey = '&key=AIzaSyBg70uBXWb-Akwa9fy8q70E91SgPhlXWok';
-				var timezoneendpoint = timezoneAPI +lat+ ',' + lng + '&timestamp='+unixsunriseTime+apikey;
+						//Call for timezone API in order to get local adjusted time for user inputted location
+						var timezoneAPI = 'https://maps.googleapis.com/maps/api/timezone/json?location='
+						var apikey = '&key=AIzaSyBg70uBXWb-Akwa9fy8q70E91SgPhlXWok';
+						timezoneAPI += lat + ',' + lng + '&timestamp=' + unixsunriseTime + apikey;
 				
-				var request2 = new XMLHttpRequest()
-				request2.open('GET', timezoneendpoint)
-				request2.onload = function(){
-					var json2 = JSON.parse(request2.responseText);
-					var dstoffset = json2.dstOffset;
-					var offset = json2.rawOffset;
-					var adjustedSunrise = unixsunriseTime + dstoffset + offset;
-					var adjustedSunriseDate = new Date(adjustedSunrise *1000);
-
-					var adjustedSunset = unixsunsetTime + dstoffset + offset;
-					var adjustedSunsetDate = new Date(adjustedSunset * 1000);
-
-// displaying time
-					var hourSunset = adjustedSunsetDate.getUTCHours() - 12;
-					var minSunset = '0'+adjustedSunsetDate.getUTCMinutes();
-					var hourtest = adjustedSunriseDate.getUTCHours(); 
-					var mintest = '0'+adjustedSunriseDate.getUTCMinutes();
+						var request2 = new XMLHttpRequest()
+						request2.open('GET', timezoneAPI)
+						request2.onload = function(){
 					
-					var sunsetDisplay = 'Sunset:' + '<br>' + hourSunset+ ':' + minSunset.substr(-2) + ' PM';
-					var sunriseDisplay = 'Sunrise:'+ '<br>'+ hourtest+':'+mintest.substr(-2) + ' AM'
-					document.getElementById('userInput').innerHTML = sunriseDisplay;
-					document.getElementById('userInput2').innerHTML = sunsetDisplay;
+							var json2 = JSON.parse(request2.responseText);
+							var dstoffset = json2.dstOffset;
+							var offset = json2.rawOffset;
 					
+							//Adjusting time from UTP to input local time
+							var adjustedSunrise = unixsunriseTime + dstoffset + offset;
+							var adjustedSunriseDate = new Date(adjustedSunrise *1000);
+
+							var adjustedSunset = unixsunsetTime + dstoffset + offset;
+							var adjustedSunsetDate = new Date(adjustedSunset * 1000);
+
+							//Display adjusted times using Date javascript library
+							var hourSunset = adjustedSunsetDate.getUTCHours() - 12;
+							var minSunset = '0'+adjustedSunsetDate.getUTCMinutes();
+							var hourSunrise = adjustedSunriseDate.getUTCHours(); 
+							var minSunrise = '0'+adjustedSunriseDate.getUTCMinutes();
+					
+							var sunsetDisplay = 'Sunset:' + '<br>' + hourSunset+ ':' + minSunset.substr(-2) + ' PM';
+							var sunriseDisplay = 'Sunrise:'+ '<br>'+ hourSunrise+':'+minSunrise.substr(-2) + ' AM'
+							document.getElementById('sunrise').innerHTML = sunriseDisplay;
+							document.getElementById('sunset').innerHTML = sunsetDisplay;
+					
+						}
+						request2.send();
+
+					}
+					request.send();
 				}
-				request2.send();
-
-}
-request.send();
-
+			});
+		}  
+	});
 
 
 
-    }
-});
 
-}  
-});
+
+
+
+
+
 
